c459cf190b91cdbf150b194ef6e61fe33236761e
TheChientleGiant <thechientlegiant@gmail.com>
2016-02-16 10:40:03 -0800

    String placeholder passing from array

--- app/src/main/java/com/shomiimages/marchingants/MainActivity.java
+++ app/src/main/java/com/shomiimages/marchingants/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
         for(String number : numbers){
-            Log.d(TAG, "Ants go marching " + number + " by " + number + ". Hoorah! Hoorah!");
+            String song = String.format("Ants go marching %1$s by %1$s. Hoorah! Hoorah!", number);
+            Log.d(TAG, song);
         }
     }
 
